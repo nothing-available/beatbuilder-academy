11ca20faf2ea4d72a26cb643e144a3f7ba71c2f2
nothing-available <smtkur31@gmail.com>
2024-04-30 15:08:29 +0530

    Hero Section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import Image from "next/image";
-
+import HeroSection from "@/components/HeroSection";
+ 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.98] antialiased bg-grid-while/[0.02]">
-      <h1 className="text-2xl text-center"> Sumit kumar</h1>
+      <HeroSection />
     </main>
   );
 }
